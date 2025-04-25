af5cf9d6875ca9d808173577c9957f4ad30d77f1
cici9787 <1321210819@qq.com>
2025-04-25 08:48:27 +0800

    5

--- code/qwen2/test2.py
+++ code/qwen2/test2.py
@@ -1,4 +1,4 @@
 from modelscope import snapshot_download
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_dir = snapshot_download("qwen/Qwen2-1.5B-Instruct", cache_dir = './')
+model_dir = snapshot_download("/root/autodl-tmp/Qwen2-1.5B-Instruct", cache_dir = './')
