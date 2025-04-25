5028199a0c75a4bde302fd3d937fe3c1a7751aab
cici9787 <1321210819@qq.com>
2025-04-25 09:09:35 +0800

    lst

--- code/qwen2/test2.py
+++ code/qwen2/test2.py
@@ -1,5 +1,6 @@
 from modelscope import snapshot_download
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 model_dir = snapshot_download("qwen/Qwen2-1.5B-Instruct", cache_dir = '/root/autodl-tmp')
 tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=False, trust_remote_code=True)
