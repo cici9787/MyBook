b0ca9d960d29aba2b4cbc0fd0049f1a316e8c8c9
cici9787 <1321210819@qq.com>
2025-04-25 13:37:43 +0800

    5

--- code/qwen2/test3.py
+++ code/qwen2/test3.py
@@ -89,17 +89,19 @@ def predict(messages, model, tokenizer):
     return response
 
 model_path = "/root/autodl-tmp"
+model_path = "/root/autodl-tmp"
 # 在modelscope上下载Qwen模型到本地目录下
-model_dir = snapshot_download(
-    "qwen/Qwen2-1.5B-Instruct", cache_dir=model_path, revision="master"
-)
+# model_dir = snapshot_download(
+#     "qwen/Qwen2-1.5B-Instruct", cache_dir=model_path, revision="master"
+# )
+model_path = "/root/autodl-tmp/qwen/Qwen2-1.5B-Instruct"
 
 # Transformers加载模型权重
 tokenizer = AutoTokenizer.from_pretrained(
-    model_dir, use_fast=False, trust_remote_code=True
+    model_path, use_fast=False, trust_remote_code=True
 )
 model = AutoModelForCausalLM.from_pretrained(
-    model_dir, device_map="auto", torch_dtype=torch.bfloat16
+    model_path, device_map="auto", torch_dtype=torch.bfloat16
 )
 model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
 
