d504aad2fb30f354d33a85dc3a75d31860bcdd07
cici9787 <1321210819@qq.com>
2025-04-26 21:35:45 +0800

    lst

---
+++ code/qwen2/train_glm4.py
@@ -0,0 +1,119 @@
+import json
+import pandas as pd
+import torch
+from datasets import Dataset
+from modelscope import snapshot_download, AutoTokenizer
+from swanlab.integration.huggingface import SwanLabCallback
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
+import os
+import swanlab
+
+
+def dataset_jsonl_transfer(origin_path, new_path):
+    """
+    将原始数据集转换为大模型微调所需数据格式的新数据集
+    """
+    messages = []
+
+    # 读取旧的JSONL文件
+    with open(origin_path, "r") as file:
+        for line in file:
+            # 解析每一行的json数据
+            data = json.loads(line)
+            context = data["text"]
+            catagory = data["category"]
+            label = data["output"]
+            message = {
+                "instruction": "你是一个文本分类领域的专家，你会接收到一段文本和几个潜在的分类选项，请输出文本内容的正确类型",
+                "input": f"文本:{context},类型选型:{catagory}",
+                "output": label,
+            }
+            messages.append(message)
+
+    # 保存重构后的JSONL文件
+    with open(new_path, "w", encoding="utf-8") as file:
+        for message in messages:
+            file.write(json.dumps(message, ensure_ascii=False) + "\n")
+
+
+def process_func(example):
+    """
+    将数据集进行预处理
+    """
+    MAX_LENGTH = 384
+    input_ids, attention_mask, labels = [], [], []
+    instruction = tokenizer(
+        f"<|system|>\n你是一个文本分类领域的专家，你会接收到一段文本和几个潜在的分类选项，请输出文本内容的正确类型<|endoftext|>\n<|user|>\n{example['input']}<|endoftext|>\n<|assistant|>\n",
+        add_special_tokens=False,
+    )
+    response = tokenizer(f"{example['output']}", add_special_tokens=False)
+    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
+    attention_mask = (
+            instruction["attention_mask"] + response["attention_mask"] + [1]
+    )
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
+    if len(input_ids) > MAX_LENGTH:  # 做一个截断
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
+
+
+def predict(messages, model, tokenizer):
+    device = "cuda"
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    print(response)
+
+    return response
+
+# model_path = "E:\\model\\"
+# data_path = 'E:/code/MyBook/code/qwen2/zh_cls_fudan-news/'
+# token_path = model_path + "ZhipuAI/glm-4-9b-chat/"
+
+model_path = "/root/autodl-tmp/"
+data_path = "./zh_cls_fudan-news/"
+token_path = model_path + "ZhipuAI/glm-4-9b-chat/"
+
+
+# 在modelscope上下载GLM模型到本地目录下
+model_dir = snapshot_download("ZhipuAI/glm-4-9b-chat", cache_dir=model_path, revision="master")
+
+# Transformers加载模型权重
+tokenizer = AutoTokenizer.from_pretrained(token_path, use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(token_path, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
+model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+
+# 加载、处理数据集和测试集
+
+train_dataset_path = data_path + "new_train.jsonl"
+test_dataset_path = data_path + "new_test.jsonl"
+
+train_jsonl_new_path = data_path + "test_new_train.jsonl"
+test_jsonl_new_path = data_path + "test_new_test.jsonl"
+
+if not os.path.exists(train_jsonl_new_path):
+    dataset_jsonl_transfer(train_dataset_path, train_jsonl_new_path)
+if not os.path.exists(test_jsonl_new_path):
+    dataset_jsonl_transfer(test_dataset_path, test_jsonl_new_path)
+
+# 得到训练集
+train_df = pd.read_json(train_jsonl_new_path, lines=True)
+train_ds = Dataset.from_pandas(train_df)
+train_dataset = train_ds.map(process_func, remove_columns=train_ds.column_names)
