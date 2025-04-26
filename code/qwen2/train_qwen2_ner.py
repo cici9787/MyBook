83cf2683aca114f5037abf3a236d80d3c656da68
cici9787 <1321210819@qq.com>
2025-04-26 13:06:26 +0800

    5

---
+++ code/qwen2/train_qwen2_ner.py
@@ -0,0 +1,113 @@
+import json
+import pandas as pd
+import torch
+from datasets import Dataset
+from modelscope import snapshot_download, AutoTokenizer
+from swanlab.integration.huggingface import SwanLabCallback
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
+import os
+import swanlab
+
+
+def dataset_jsonl_transfer(origin_path, new_path):
+    """
+    将原始数据集转换为大模型微调所需数据格式的新数据集
+    """
+    messages = []
+
+    # 读取旧的JSONL文件
+    with open(origin_path, "r") as file:
+        for line in file:
+            # 解析每一行的json数据
+            data = json.loads(line)
+            input_text = data["text"]
+            entities = data["entities"]
+            match_names = ["地点", "人名", "地理实体", "组织"]
+
+            entity_sentence = ""
+            for entity in entities:
+                entity_json = dict(entity)
+                entity_text = entity_json["entity_text"]
+                entity_names = entity_json["entity_names"]
+                for name in entity_names:
+                    if name in match_names:
+                        entity_label = name
+                        break
+
+                entity_sentence += f"""{{"entity_text": "{entity_text}", "entity_label": "{entity_label}"}}"""
+
+            if entity_sentence == "":
+                entity_sentence = "没有找到任何实体"
+
+            message = {
+                "instruction": """你是一个文本实体识别领域的专家，你需要从给定的句子中提取 地点; 人名; 地理实体; 组织 实体. 以 json 格式输出, 如 {"entity_text": "南京", "entity_label": "地理实体"} 注意: 1. 输出的每一行都必须是正确的 json 字符串. 2. 找不到任何实体时, 输出"没有找到任何实体". """,
+                "input": f"文本:{input_text}",
+                "output": entity_sentence,
+            }
+
+            messages.append(message)
+
+    # 保存重构后的JSONL文件
+    with open(new_path, "w", encoding="utf-8") as file:
+        for message in messages:
+            file.write(json.dumps(message, ensure_ascii=False) + "\n")
+
+
+def process_func(example):
+    """
+    将数据集进行预处理, 处理成模型可以接受的格式
+    """
+
+    MAX_LENGTH = 384
+    input_ids, attention_mask, labels = [], [], []
+    system_prompt = """你是一个文本实体识别领域的专家，你需要从给定的句子中提取 地点; 人名; 地理实体; 组织 实体. 以 json 格式输出, 如 {"entity_text": "南京", "entity_label": "地理实体"} 注意: 1. 输出的每一行都必须是正确的 json 字符串. 2. 找不到任何实体时, 输出"没有找到任何实体"."""
+
+    instruction = tokenizer(
+        f"<|im_start|>system\n{system_prompt}<|im_end|>\n<|im_start|>user\n{example['input']}<|im_end|>\n<|im_start|>assistant\n",
+        add_special_tokens=False,
+    )
+    response = tokenizer(f"{example['output']}", add_special_tokens=False)
+    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
+    attention_mask = (
+            instruction["attention_mask"] + response["attention_mask"] + [1]
+    )
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
+    if len(input_ids) > MAX_LENGTH:  # 做一个截断
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
+
+
+def predict(messages, model, tokenizer):
+    device = "cuda"
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    print(response)
+
+    return response
+
+model_id = "qwen/Qwen2-1.5B-Instruct"
+model_dir = "./qwen/Qwen2-1___5B-Instruct"
+
+model_path = "/root/autodl-tmp/"
+data_path = "./zh_cls_fudan-news/"
+token_path = model_path + "/qwen/Qwen2-1___5B-Instruct/"
+
+model_dir = snapshot_download(model_id, cache_dir=model_path, revision="master")
