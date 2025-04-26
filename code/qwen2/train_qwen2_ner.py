9acdeaefa01218853e5a754d2972db182f203566
cici9787 <1321210819@qq.com>
2025-04-26 13:59:18 +0800

    6

--- code/qwen2/train_qwen2_ner.py
+++ code/qwen2/train_qwen2_ner.py
@@ -128,3 +128,47 @@ print("train_df:", train_df)
 train_ds = Dataset.from_pandas(train_df)
 train_dataset = train_ds.map(process_func, remove_columns=train_ds.column_names)
 print("train_dataset:", train_dataset)
+
+config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+    inference_mode=False,  # 训练模式
+    r=8,  # Lora 秩
+    lora_alpha=32,  # Lora alaph，具体作用参见 Lora 原理
+    lora_dropout=0.1,  # Dropout 比例
+)
+
+model = get_peft_model(model, config)
+
+args = TrainingArguments(
+    output_dir="./output/Qwen2-NER",
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    gradient_accumulation_steps=4,
+    logging_steps=10,
+    num_train_epochs=2,
+    save_steps=100,
+    learning_rate=1e-4,
+    save_on_each_node=True,
+    gradient_checkpointing=True,
+    report_to="none",
+)
+
+swanlab_callback = SwanLabCallback(
+    project="Qwen2-NER-fintune",
+    experiment_name="Qwen2-1.5B-Instruct",
+    description="使用通义千问Qwen2-1.5B-Instruct模型在NER数据集上微调，实现关键实体识别任务。",
+    config={
+        "model": model_id,
+        "model_dir": model_dir,
+        "dataset": "qgyd2021/chinese_ner_sft",
+    },
+)
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
+    callbacks=[swanlab_callback],
+)
